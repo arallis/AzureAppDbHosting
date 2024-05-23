33754abcd291e12656aabb6bea155a16030bd18f
alexi <alexi@DESKTOP-37746U8>
2024-05-23 07:32:03 +0100

    Add project files.

---
+++ AzureAppDbHosting/Models/LeadEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureAppDbHosting.Models
+{
+    public class LeadEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayName("Date")]
+
+        public DateTime LeadDate { get; set; }
+
+        public string OwnerWalker { get; set; }
+
+        public string Name { get; set; }
+
+        public string Mobile {  get; set; }
+
+        public string Email { get; set; }
+    }
+}
